9bedd5eca860c1d51a5b1fd4ca1fc64cc5c9880c
Andy Tham <andytham1324@gmail.com>
2023-03-26 18:17:25 +0100

    app built

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const path = require('path');
 
 module.exports = {
-  
   images: {
-    domains: ["image.tmdb.org","www.freepnglogos.com"],
-
+    domains: ["image.tmdb.org", "www.freepnglogos.com"],
   },
- 
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add the path to the redux folder to the list of modules that webpack will resolve
+    config.resolve.modules.push(path.resolve(__dirname, './redux'));
+
+    // Return the modified config
+    return config;
+  },
 };
